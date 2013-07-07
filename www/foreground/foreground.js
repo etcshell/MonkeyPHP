8e72b7ecc6c626c62d62fc437ff903680abe78ec
shalalal <shalalal@126.com>
2013-07-07 17:13:46 +0800

    常规提交

--- www/foreground/foreground.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: Administrator
- * Date: 13-5-16
- * Time: 下午7:57
- * To change this template use File | Settings | File Templates.
- */
--- www/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * 整个MKCMS的唯一入口文件
- */
-//定义前端路径
-define('FRONT_FILE', strtr(__FILE__,DIRECTORY_SEPARATOR,'/') );
-//将用户请求交给启动程序。
-require(dirname(FRONT_FILE).'/../system/Applications/Default/Application.php');
