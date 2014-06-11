17f0a9fc453b2f5593d28a8fbcb2901e658cc84a
shalalal <shalalal@126.com>
2014-06-11 19:57:31 +0800

    将Library移出MonkeyPHP框架，尊重各个库类的原创版权（虽然我有优化整合）。

--- system/vendor/composer/autoload_namespaces.php
+++ system/vendor/composer/autoload_namespaces.php
@@ -8,4 +8,6 @@ $baseDir = dirname($vendorDir);
 return array(
     'Monkey' => array($vendorDir .DIRECTORY_SEPARATOR. 'monkey'),
     'Markdown' => array($vendorDir .DIRECTORY_SEPARATOR. 'markdown'),
+    'Library' => array($vendorDir .DIRECTORY_SEPARATOR. 'library'),
+
 );
